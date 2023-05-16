d131c4300cb3c94c3862a32446c96ae0d72a304b
harryd57 <harryd57@yahoo.com>
2023-05-16 05:18:57 +0100

    about and contact components created

--- Angular/portfolio/src/app/app.module.ts
+++ Angular/portfolio/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
